73d649d8b5dc415c0a71f6783f50f5de3dcbecf3
Arsene_Fx <duplexdissi00@gmail.com>
2022-02-09 00:36:15 -0500

    Debut de notre projet

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Numero 3 
+function long(a) {
+
+    return console.log(a.length);
+}
+
+long('Arsene');
+console.log(long);
+
+
+4
+function effac(a, b) {
+
+    var a = "maman";
+    var b = a.replace(/a/g, "");
+
+    return console.log(b);
+}  
+
+*/
+
+
+function min() {
+
+    var a = [3, 2, 1, 5, 6, 4];
+
+    var b = a.sort();
+
+    return console.log(b[1]);
+}
+
+console.log(min());
